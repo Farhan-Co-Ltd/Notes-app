f89ee56d19d6a1af903971eed5716a03819fb5f3
Farhan-Co-Ltd <alfarizyfarhan468@gmail.com>
2023-01-10 23:59:31 +0700

    Membuat migration Create Table Users dan testing Authentication

--- notes-app/src/server.js
+++ notes-app/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
 const notes = require('./api/notes')
+const ClientError = require('./exceptions/ClientError')
 const NotesService = require('./services/postgres/NotesService')
 const NotesValidator = require('./validator/notes')
 
@@ -23,6 +24,34 @@ const init = async () => {
       service: notesService,
       validator: NotesValidator
     }
+  })
+
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request
+    if (response instanceof Error) {
+      // penanganan client error secara internal.
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message
+        })
+        newResponse.code(response.statusCode)
+        return newResponse
+      }
+      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+      if (!response.isServer) {
+        return h.continue
+      }
+      // penanganan server error sesuai kebutuhan
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami'
+      })
+      newResponse.code(500)
+      return newResponse
+    }
+    return h.continue
   })
 
   await server.start()
