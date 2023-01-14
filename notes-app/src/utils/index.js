1e2ae8951965613af4958aa33893881741dfc39c
Farhan-Co-Ltd <alfarizyfarhan468@gmail.com>
2023-01-15 02:15:23 +0700

    Implementasi Collaboration Features

--- notes-app/src/utils/index.js
+++ notes-app/src/utils/index.js
@@ -4,14 +4,16 @@ const mapDBToModel = ({
   body,
   tags,
   created_at, // eslint-disable-line
-  updated_at // eslint-disable-line
+  updated_at, // eslint-disable-line
+  username
 }) => ({
   id,
   title,
   body,
   tags,
   createdAt: created_at, // eslint-disable-line
-  updatedAt: updated_at // eslint-disable-line
+  updatedAt: updated_at, // eslint-disable-line
+  username
 })
 
 module.exports = { mapDBToModel }
